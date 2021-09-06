85d73ec6c3d84fe2bdf78eaf6d2f5332169e9368
Safayet Jamil <work.jamil4@gmail.com>
2021-09-07 00:50:50 +0600

    Jamil Console Error Fixed
    	1:Some console error fix
    	2:Still there is one.

--- src/components/User-Dashboard/apply-jobs/JobList.js
+++ src/components/User-Dashboard/apply-jobs/JobList.js
@@ -32,7 +32,7 @@ function JobList() {
                                 <td className="pl-4 cursor-pointer">
                                     <div className="flex items-center">
                                         <div className="w-10 h-10">
-                                            <img className="w-full h-full" src="https://cdn.tuk.dev/assets/templates/olympus/projects.png" />
+                                            <img className="w-full h-full" alt="image" src="https://cdn.tuk.dev/assets/templates/olympus/projects.png" />
                                         </div>
                                         <div className="pl-4">
                                             <p className="font-medium">UX Design &amp; Visual Strategy</p>
@@ -58,9 +58,9 @@ function JobList() {
                                     <p className="font-medium">22.12.21</p>
                                     <p className="text-xs leading-3 text-gray-600 mt-2">34 days</p>
                                 </td>
-                                <td className="px-7 text-center 2xl:px-0">
+                                <td className="px-7 2xl:px-0">
                                     {
-                                        show==0 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
+                                        show===0 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
                                         <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 20 20" fill="none">
                                             <path d="M4.16667 10.8334C4.62691 10.8334 5 10.4603 5 10.0001C5 9.53984 4.62691 9.16675 4.16667 9.16675C3.70643 9.16675 3.33334 9.53984 3.33334 10.0001C3.33334 10.4603 3.70643 10.8334 4.16667 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M10 10.8334C10.4602 10.8334 10.8333 10.4603 10.8333 10.0001C10.8333 9.53984 10.4602 9.16675 10 9.16675C9.53976 9.16675 9.16666 9.53984 9.16666 10.0001C9.16666 10.4603 9.53976 10.8334 10 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
@@ -74,7 +74,7 @@ function JobList() {
                                         </svg>
                                     </button>
                                     }
-                                   {show==0 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
+                                   {show===0 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
                                         <div className="text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
                                             <p>Edit</p>
                                         </div>
@@ -117,7 +117,7 @@ function JobList() {
                               
                                 <td className="px-7 text 2xl:px-0">
                                     {
-                                        show==1 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
+                                        show===1 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
                                         <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 20 20" fill="none">
                                             <path d="M4.16667 10.8334C4.62691 10.8334 5 10.4603 5 10.0001C5 9.53984 4.62691 9.16675 4.16667 9.16675C3.70643 9.16675 3.33334 9.53984 3.33334 10.0001C3.33334 10.4603 3.70643 10.8334 4.16667 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M10 10.8334C10.4602 10.8334 10.8333 10.4603 10.8333 10.0001C10.8333 9.53984 10.4602 9.16675 10 9.16675C9.53976 9.16675 9.16666 9.53984 9.16666 10.0001C9.16666 10.4603 9.53976 10.8334 10 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
@@ -131,7 +131,7 @@ function JobList() {
                                         </svg>
                                     </button>
                                     }
-                                   {show==1 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
+                                   {show===1 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
                                         <div className="text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
                                             <p>Edit</p>
                                         </div>
@@ -174,7 +174,7 @@ function JobList() {
                                 
                                 <td className="px-7 2xl:px-0">
                                     {
-                                        show==2 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
+                                        show===2 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
                                         <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 20 20" fill="none">
                                             <path d="M4.16667 10.8334C4.62691 10.8334 5 10.4603 5 10.0001C5 9.53984 4.62691 9.16675 4.16667 9.16675C3.70643 9.16675 3.33334 9.53984 3.33334 10.0001C3.33334 10.4603 3.70643 10.8334 4.16667 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M10 10.8334C10.4602 10.8334 10.8333 10.4603 10.8333 10.0001C10.8333 9.53984 10.4602 9.16675 10 9.16675C9.53976 9.16675 9.16666 9.53984 9.16666 10.0001C9.16666 10.4603 9.53976 10.8334 10 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
@@ -188,7 +188,7 @@ function JobList() {
                                         </svg>
                                     </button>
                                     }
-                                   {show==2 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
+                                   {show===2 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
                                         <div className="text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
                                             <p>Edit</p>
                                         </div>
@@ -231,7 +231,7 @@ function JobList() {
                                 
                                 <td className="px-7 2xl:px-0">
                                     {
-                                        show==3 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
+                                        show===3 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
                                         <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 20 20" fill="none">
                                             <path d="M4.16667 10.8334C4.62691 10.8334 5 10.4603 5 10.0001C5 9.53984 4.62691 9.16675 4.16667 9.16675C3.70643 9.16675 3.33334 9.53984 3.33334 10.0001C3.33334 10.4603 3.70643 10.8334 4.16667 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M10 10.8334C10.4602 10.8334 10.8333 10.4603 10.8333 10.0001C10.8333 9.53984 10.4602 9.16675 10 9.16675C9.53976 9.16675 9.16666 9.53984 9.16666 10.0001C9.16666 10.4603 9.53976 10.8334 10 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
@@ -245,7 +245,7 @@ function JobList() {
                                         </svg>
                                     </button>
                                     }
-                                   {show==3 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
+                                   {show===3 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
                                         <div className="text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
                                             <p>Edit</p>
                                         </div>
@@ -288,7 +288,7 @@ function JobList() {
                               
                                 <td className="px-7 2xl:px-0">
                                     {
-                                        show==4 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
+                                        show===4 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
                                         <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 20 20" fill="none">
                                             <path d="M4.16667 10.8334C4.62691 10.8334 5 10.4603 5 10.0001C5 9.53984 4.62691 9.16675 4.16667 9.16675C3.70643 9.16675 3.33334 9.53984 3.33334 10.0001C3.33334 10.4603 3.70643 10.8334 4.16667 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M10 10.8334C10.4602 10.8334 10.8333 10.4603 10.8333 10.0001C10.8333 9.53984 10.4602 9.16675 10 9.16675C9.53976 9.16675 9.16666 9.53984 9.16666 10.0001C9.16666 10.4603 9.53976 10.8334 10 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
@@ -302,7 +302,7 @@ function JobList() {
                                         </svg>
                                     </button>
                                     }
-                                   {show==4 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
+                                   {show===4 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
                                         <div className="text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
                                             <p>Edit</p>
                                         </div>
@@ -345,7 +345,7 @@ function JobList() {
                                 
                                 <td className="px-7 2xl:px-0">
                                     {
-                                        show==5 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
+                                        show===5 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
                                         <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 20 20" fill="none">
                                             <path d="M4.16667 10.8334C4.62691 10.8334 5 10.4603 5 10.0001C5 9.53984 4.62691 9.16675 4.16667 9.16675C3.70643 9.16675 3.33334 9.53984 3.33334 10.0001C3.33334 10.4603 3.70643 10.8334 4.16667 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M10 10.8334C10.4602 10.8334 10.8333 10.4603 10.8333 10.0001C10.8333 9.53984 10.4602 9.16675 10 9.16675C9.53976 9.16675 9.16666 9.53984 9.16666 10.0001C9.16666 10.4603 9.53976 10.8334 10 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
@@ -359,7 +359,7 @@ function JobList() {
                                         </svg>
                                     </button>
                                     }
-                                   {show==5 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
+                                   {show===5 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
                                         <div className="text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
                                             <p>Edit</p>
                                         </div>
@@ -402,7 +402,7 @@ function JobList() {
                                
                                 <td className="px-7 2xl:px-0">
                                     {
-                                        show==6 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
+                                        show===6 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
                                         <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 20 20" fill="none">
                                             <path d="M4.16667 10.8334C4.62691 10.8334 5 10.4603 5 10.0001C5 9.53984 4.62691 9.16675 4.16667 9.16675C3.70643 9.16675 3.33334 9.53984 3.33334 10.0001C3.33334 10.4603 3.70643 10.8334 4.16667 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M10 10.8334C10.4602 10.8334 10.8333 10.4603 10.8333 10.0001C10.8333 9.53984 10.4602 9.16675 10 9.16675C9.53976 9.16675 9.16666 9.53984 9.16666 10.0001C9.16666 10.4603 9.53976 10.8334 10 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
@@ -416,7 +416,7 @@ function JobList() {
                                         </svg>
                                     </button>
                                     }
-                                   {show==6 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
+                                   {show===6 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
                                         <div className="text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
                                             <p>Edit</p>
                                         </div>
@@ -459,7 +459,7 @@ function JobList() {
                                 
                                 <td className="px-7 2xl:px-0">
                                     {
-                                        show==7 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
+                                        show===7 ? <button onClick={()=>setShow(null)} className="focus:outline-none pl-7">
                                         <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 20 20" fill="none">
                                             <path d="M4.16667 10.8334C4.62691 10.8334 5 10.4603 5 10.0001C5 9.53984 4.62691 9.16675 4.16667 9.16675C3.70643 9.16675 3.33334 9.53984 3.33334 10.0001C3.33334 10.4603 3.70643 10.8334 4.16667 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M10 10.8334C10.4602 10.8334 10.8333 10.4603 10.8333 10.0001C10.8333 9.53984 10.4602 9.16675 10 9.16675C9.53976 9.16675 9.16666 9.53984 9.16666 10.0001C9.16666 10.4603 9.53976 10.8334 10 10.8334Z" stroke="#A1A1AA" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
@@ -473,7 +473,7 @@ function JobList() {
                                         </svg>
                                     </button>
                                     }
-                                   {show==7 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
+                                   {show===7 &&  <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
                                         <div className="text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
                                             <p>Edit</p>
                                         </div>
