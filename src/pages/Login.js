a6113bae42d7a0cb8f636ad266937fb80f19890d
Safayet Jamil <work.jamil4@gmail.com>
2021-12-29 21:47:31 +0600

    job manager
    	1:login register basic function
    	2:working login logout

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,43 @@
 import { LockClosedIcon } from '@heroicons/react/solid'
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axiosInstance from '../axios';
+export default function SignIn() {
+  const history = useHistory();
+	const initialFormData = Object.freeze({
+		email: '',
+		password: '',
+	});
 
-export default function SignUp() {
+	const [formData, updateFormData] = useState(initialFormData);
+
+	const handleChange = (e) => {
+		updateFormData({
+			...formData,
+			[e.target.name]: e.target.value.trim(),
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formData);
+
+		axiosInstance
+			.post(`token/`, {
+				email: formData.email,
+				password: formData.password,
+			})
+			.then((res) => {
+				localStorage.setItem('access_token', res.data.access);
+				localStorage.setItem('refresh_token', res.data.refresh);
+				axiosInstance.defaults.headers['Authorization'] =
+					'JWT ' + localStorage.getItem('access_token');
+				history.push('/');
+				//console.log(res);
+				//console.log(res.data);
+			});
+	};
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
@@ -13,9 +50,9 @@ export default function SignUp() {
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{' '}
-            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+            {/* <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
               start your 14-day free trial
-            </a>
+            </a> */}
           </p>
         </div>
         <form className="mt-8 space-y-6" action="#" method="POST">
@@ -26,13 +63,14 @@ export default function SignUp() {
                 Email address
               </label>
               <input
-                id="email-address"
+                id="email"
+                label="Email Address"
                 name="email"
-                type="email"
                 autoComplete="email"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -40,13 +78,15 @@ export default function SignUp() {
                 Password
               </label>
               <input
-                id="password"
                 name="password"
+                label="Password"
                 type="password"
+                id="password"
                 autoComplete="current-password"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -65,14 +105,15 @@ export default function SignUp() {
             </div>
 
             <div className="text-sm">
-              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+              {/* <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
                 Forgot your password?
-              </a>
+              </a> */}
             </div>
           </div>
 
           <div>
             <button
+              onClick={handleSubmit}
               type="submit"
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
